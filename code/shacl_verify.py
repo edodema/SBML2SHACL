68b7044d82a6dcb02e9be31d1d14ef4e99e25e67
edodema <edodema13@gmail.com>
2020-07-31 18:34:58 +0200

    improved command line arguments management

--- code/shacl_verify.py
+++ code/shacl_verify.py
@@ -1,28 +1,18 @@
-import sys
+import argparse
 from pyshacl import validate
-from os import path
 
-shapes_file = "./code/shapes.ttl"
-data_file = "./code/output.ttl"
+# Command line arguments
+parser = argparse.ArgumentParser()
+   
+parser.add_argument('-s', '--shapes', required=True, dest='shapes_file', metavar='shapes', help='shapes file')
+parser.add_argument('-d', '--data', required=True, dest='data_file', metavar='data', help='data file')
 
-conforms, v_graph, v_text = validate(data_file, shacl_graph=shapes_file,
+args = parser.parse_args()
+
+# Validation 
+conforms, v_graph, v_text = validate(args.data_file, shacl_graph=args.shapes_file,
    inference='rdfs', debug=True, serialize_report_graph=True)
 
-#print(conforms)
+print(conforms)
 #print(v_graph)
 print(v_text)
-
-######## command line version, use this in the final version
-#if len(sys.argv) != 3: 
-#    print('help: insert shape file as first argument and data file as the second one.')
-#    exit(1)
-
-#shapes_file = sys.argv[1]
-#data_file = sys.argv[2]
-
-#conforms, v_graph, v_text = validate(data_file, shacl_graph=shapes_file,
-#   inference='rdfs', debug=True, serialize_report_graph=True)
-
-#print(conforms)
-#print(v_graph)
-#print(v_text)
