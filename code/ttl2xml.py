72069e53ee351812a7ac69b6d9f1dfb2c3771807
edodema <edodema13@gmail.com>
2020-07-31 23:21:35 +0200

    converter from .ttl to .xml

---
+++ code/ttl2xml.py
@@ -0,0 +1,18 @@
+import rdflib.graph as g
+import argparse
+
+# Command line arguments
+parser = argparse.ArgumentParser()
+   
+parser.add_argument('-f', '--file', required=True, dest='input_file', metavar='file', help='model file')
+parser.add_argument('-o', '--output', required=True, dest='output_file', metavar='output', help='output file')
+
+args = parser.parse_args()
+
+# FIle IO
+output = open(args.output_file, 'w')
+
+# Query 
+graph = g.Graph()
+graph.parse(args.input_file, format='ttl')
+output.write(graph.serialize(format='pretty-xml').decode('utf-8'))
